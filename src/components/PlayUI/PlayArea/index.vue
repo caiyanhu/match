61d62856e4a79a8b4a077a176268450a6901de3c
caiyanhu <caiyanhu@cmss.chinamobile.com>
2024-12-18 22:39:33 +0800

    feat: WIP 游戏主页
    Code Source From: Self Code
    Description:
    Jira:
    市场项目编号（名称）:

---
+++ src/components/PlayUI/PlayArea/index.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import AffixIcon from "./assets/affix.svg";
+</script>
+
+<template>
+  <div class="play-area">
+    <div class="left-top">
+      <AffixIcon />
+    </div>
+    <div class="right-top">
+      <AffixIcon />
+    </div>
+    <div class="left-bottom">
+      <AffixIcon />
+    </div>
+    <div class="right-bottom">
+      <AffixIcon />
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.play-area {
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 800px;
+  height: 800px;
+  border-radius: 12px;
+
+  position: absolute;
+  top: 189px;
+  left: 567px;
+
+  .left-top {
+    position: absolute;
+    top: 25px;
+    left: 28px;
+  }
+
+  .left-bottom {
+    position: absolute;
+    bottom: 25px;
+    left: 28px;
+  }
+
+  .right-top {
+    position: absolute;
+    top: 25px;
+    right: 28px;
+  }
+
+  .right-bottom {
+    position: absolute;
+    bottom: 25px;
+    right: 28px;
+  }
+}
+</style>
---
+++ src/components/PlayUI/Toolbox/toolItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tool-item">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.tool-item {
+  width: 138px;
+  height: 138px;
+  background-image: url("./assets/background.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-right: 34px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+}
+</style>
